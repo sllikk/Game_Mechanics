7eba4d6a9c5624531f59c60d32f585fd0566b541
SL1ick <fddftrrfg@gmail.com>
2024-11-21 00:18:04 +0200

    Start_Dev

---
+++ Source/Mechanics/Private/Game/Base_GameMode.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Mechanics/Public/Game/Base_GameMode.h"
+
+ABase_GameMode::ABase_GameMode()
+{
+	
+}
---
+++ Source/Mechanics/Public/Game/Base_GameMode.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Base_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Config=Game)
+class MECHANICS_API ABase_GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:	
+
+	ABase_GameMode();
+	
+};
