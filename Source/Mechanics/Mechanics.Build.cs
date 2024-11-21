bfda3e3afabe7cddf53149f86f2d1fb03621c6d8
SL1ick <fddftrrfg@gmail.com>
2024-11-21 19:03:29 +0200

    Dlss and Reflex

--- Source/Mechanics/Mechanics.Build.cs
+++ Source/Mechanics/Mechanics.Build.cs
@@ -8,7 +8,7 @@ public class Mechanics : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "DeveloperSettings", "UMG", "DLSS", });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
